bf8723fb8209632f1c909799861ed4c7c936d44f
suyoin <19378617+suyoin@users.noreply.github.com>
2021-10-26 22:05:08 -0500

    export DeepWritable and DeepReadonly

--- src/table.d.ts
+++ src/table.d.ts
@@ -1,4 +1,4 @@
-type DeepWritable<T> = T extends ReadonlyMap<infer K, infer V>
+export type DeepWritable<T> = T extends ReadonlyMap<infer K, infer V>
 	? Map<K, DeepWritable<V>>
 	: T extends ReadonlyArray<infer E>
 	? Array<DeepWritable<E>>
@@ -7,6 +7,18 @@ type DeepWritable<T> = T extends ReadonlyMap<infer K, infer V>
 		? T
 		: {
 				-readonly [P in keyof T]: DeepWritable<T[P]>;
+		  }
+	: T;
+
+export type DeepReadonly<T> = T extends Map<infer K, infer V>
+	? ReadonlyMap<K, DeepReadonly<V>>
+	: T extends Array<infer E>
+	? ReadonlyArray<DeepReadonly<E>>
+	: T extends object
+	? keyof T extends never
+		? T
+		: {
+				-readonly [P in keyof T]: DeepReadonly<T[P]>;
 		  }
 	: T;
 
